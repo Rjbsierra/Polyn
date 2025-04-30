cbb63955c3f90c5b3d834232c7d12b88151e7384
RC James Sierra <rjbsierra@gmail.com>
2025-04-30 10:17:15 +0800

    designed settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { RankComponent } from './pages/rank/rank.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent
+    component: SettingsComponent
   },
   {
     path: "home",
